cb24d5bb1e3fa4bb935d6b043ea6674167de8b05
Brant Peterson <brantp@gmail.com>
2012-07-28 17:07:30 -0400

    fixed vcf_to_rqtl short_read_analysis dependency

--- vcf_to_rqtl.py
+++ vcf_to_rqtl.py
@@ -1,7 +1,306 @@
 #!/usr/bin/env python
+'''invocation:
+vcf_to_rqtl.py path_to_vcf.vcf Q G
+where:
+Q = GATK "QD" score threshold for inclusion
+G = individual genotype call quality threshold
 
-from short_read_analysis import variant_detection,extract_genotypes_from_mclgr
+'''
+# functionality from short_read now copied here
+#from short_read_analysis import variant_detection,extract_genotypes_from_mclgr
+
 import os,sys
+from collections import defaultdict
+
+def hwe(indiv_gt):
+    '''given an indiv_gt dict as in vcf_data
+
+    returns the observed and expected classes for hardy-weinberg equilibrium'''
+
+    alleles = set(reduce(lambda x,y:x+y,[re.split('[/|]',v['GT']) for v in indiv_gt.values()]))
+    if len(alleles) == 1:
+        return 0
+    if len(alleles) > 2:
+        return None
+
+    Aid, Bid = sorted(list(alleles))
+
+    A = 0
+    a = 0
+
+    AA = 0
+    Aa = 0
+    aa = 0
+
+    for v in indiv_gt.values():
+        if set(re.split('[/|]',v['GT'])) == set([Aid]):
+            AA += 1
+            A += 2
+        elif set(re.split('[/|]',v['GT'])) == set([Aid,Bid]):
+            Aa += 1
+            A += 1
+            a += 1
+        elif set(re.split('[/|]',v['GT'])) == set([Bid]):
+            aa += 1
+            a += 2
+    try:
+        p = A/float(A+a)
+    except ZeroDivisionError:
+        print >> sys.stderr, indiv_gt
+    q = 1-p
+
+    tot = AA+Aa+aa
+
+    expAA = p**2 * tot
+    expAa = 2*p*q*tot
+    expaa = q**2 *tot
+
+    X2 = sum([(o-e)**2/e for o,e in zip([AA,Aa,aa],[expAA,expAa,expaa]) if e != 0])
+
+    return X2
+        
+def maf(indiv_gt):
+    '''returns the counts of the minor allele for an indiv_gt dict as in vcf_data above'''
+
+    A = 0
+    a = 0
+
+    for v in indiv_gt.values():
+        A += v['GT'].count('0')
+        a += v['GT'].count('1')
+
+    return min(A,a)
+
+def fract_het(indiv_gt):
+    gts = [v['GT'] for v in indiv_gt.values()]
+
+    return (gts.count('0/1') + gts.count('0|1') + gts.count('1|0'))/float(len(gts))
+
+def load_vcf(vcf,cutoff_fn=None,ding_on=100000,store_only=None,indiv_gt_phred_cut=None,store_indiv_prefix=None,drop_indiv=None,biallelic_sites=True,skip_noGQ=True):
+    '''populates and returns a site:properties dict from vcf file
+
+    if store_only is set, must be a list of fields to retain
+
+    if cutoff_fn is set, each sd (dict of fully parsed line, subject to store_only filter) is passed to cutoff_fn
+    must be callable, and truth value of the return determines retention of that site
+    '''
+
+    vcf_data = {}
+    
+    i = 0
+    for line in open(vcf):
+        
+        if i % ding_on == 0: print >> sys.stderr, 'reading',i
+        i += 1
+
+        #skip bogus lines -- probably want to warn on this somehow...
+        if '\x00' in line:
+            print >> sys.stderr, 'Null byte in line %s, skipping' % i
+            continue
+
+        if line.startswith('#CHROM'):
+            if write_thresholded_vcf is not None:
+                ofh.write(line)
+            headers = line[1:].split()
+            exp_elements = len(line.split())
+            FORMAT = headers.index('FORMAT')
+            if write_fasta_base is not None: #DON'T INCLUDE DROP_INDIV
+                if drop_indiv is not None:
+                    indivs = list(set(headers[FORMAT+1:]) - set(drop_indiv))
+                else:
+                    indivs = headers[FORMAT+1:]
+        elif line.startswith('#'):
+            continue
+        else:
+            #extract site stats
+            fields = line.split()
+            if len(fields) != exp_elements:
+                print >>sys.stderr, 'unexpected length, line %s (exp %s obs %s)' % (i,exp_elements,len(fields))
+                continue
+
+            sd = dict(zip(headers[:FORMAT],fields[:FORMAT]))
+            key = (sd['CHROM'],sd['POS'])
+            try:
+                infostr = sd.pop('INFO')
+                sd.update(dict([el.split('=') for el in infostr.split(';') if '=' in el]))
+            except KeyError:
+                pass
+
+            if biallelic_sites is True:
+                if ',' in sd['ALT']:
+                    continue
+
+            sd['indiv_gt'] = {}
+            for ind,gt in zip(headers[FORMAT+1:],fields[FORMAT+1:]):
+                if store_indiv_prefix is not None and not ind.startswith(store_indiv_prefix): continue
+                #drop_indiv HERE
+                if drop_indiv is not None and ind in drop_indiv: continue
+                if ':' in gt:
+                    try:
+                        this_gt = dict(zip(fields[FORMAT].split(':'),gt.split(':')))
+                        if indiv_gt_phred_cut is None or float(this_gt['GQ']) >= indiv_gt_phred_cut:
+                            sd['indiv_gt'][ind] = this_gt
+                    except:
+                        if skip_noGQ:
+                            pass
+                        else:
+                            print >> sys.stderr, 'parse failed for genotype string:\n\n',this_gt,'\n'
+                            raise
+
+            if len(sd['indiv_gt']) > 0: 
+                sd['hwe'] = hwe(sd['indiv_gt'])
+                sd['mac'] = maf(sd['indiv_gt'])
+                sd['maf'] = sd['mac'] / (2. * len(sd['indiv_gt']))
+                if sd['hwe'] is None:
+                    sd['mafbyhwe'] = None
+                else:
+                    sd['mafbyhwe'] = sd['maf'] / numpy.log1p(sd['hwe'])
+                sd['fh'] = fract_het(sd['indiv_gt'])
+                sd['totcov'] = sum([int(gtd['DP']) for gtd in sd['indiv_gt'].values()])
+                sd['numind'] = len(sd['indiv_gt'])
+                
+            else:
+                continue #this is questionable -- ever want to proceed with a genotype-less site?
+                
+            if cutoff_fn is None or cutoff_fn(sd):
+                if store_only is not None:
+                    keep_sd = {}
+                    for k in sd:
+                        if k in store_only:
+                            keep_sd[k] = sd[k]
+                    sd = keep_sd
+                if len(sd) > 0:
+                    vcf_data[key] = sd
+                if write_thresholded_vcf is not None:
+                    ofh.write('\t'.join(fields) + '\n')
+
+    return vcf_data
+
+def genotypes_from_vcf_obj(vcf,min_indiv=40):
+    pm = {}
+    gt = defaultdict(dict)
+    for k,v in vcf.items():
+        if len(v['indiv_gt']) < min_indiv or v['mac'] == 0: continue
+        sname = '%s.%s' % k
+        pm[sname] = [v['REF'],v['ALT']]
+        for ind,gtdict in v['indiv_gt'].items():
+            gt[ind][sname] = [int(i) for i in gtdict['GT'].split('/')]
+
+    return pm,gt
+
+def genotypes_by_parent(pm,gt,parents,hybrids=None,remove_targets=None):
+    '''Given pm and gt per tabulate_genotypes,
+    and dictionary parents of the form
+    {'A': ['BW1','BW2']...}
+    + optional list hybrids
+    returns loci and genotypes suitable for output_cross_radtag_genotypes
+    + a list of target individuals to remove (e.g. parents, problematic indivs)'''
+
+    print >> sys.stderr, '%s snps to evalute' % (len(pm))
+
+    fixedsnps = {}
+    for site in pm.keys():
+        gts = defaultdict(list)
+        for pk in parents.keys():
+            for indiv in parents[pk]:
+                try:
+                    gts[pk].extend(gt[indiv].get(site,[]))
+                except:
+                    print indiv,site
+                    raise
+        gts['A'] = list(set(gts['A']))
+        gts['B'] = list(set(gts['B']))
+        if len(gts['A']) == 1 and len(gts['B']) == 1 and gts['A'][0] != gts['B'][0]:
+            gtlookup = {}
+            for pk in gts.keys():
+                gtlookup[gts[pk][0]] = pk
+            fixedsnps[site] = gtlookup
+    print >> sys.stderr, '%s fixed snps between parents' % (len(fixedsnps))
+    
+    if hybrids:
+        f1het = []
+        for site in fixedsnps.keys():
+            #print >>sys.stderr,[gt[polarized_loci,polarized_geno = extract_genotypes_from_mclgr.genotypes_by_parent(pm,gt,parents,hybrids=hybrids,remove_targets=reduce(lambda x,y: x+y,parents.values()) + hybrids)h][site] for h in hybrids if gt[h][site]]
+            #print >>sys.stderr,[(len(gt[h][site]), set([fixedsnps[site][gt[h][site][i]] for i in range(2)])) for h in hybrids if gt[h][site]]
+            if all([len(gt[h].get(site,[]))==2 and set(['A','B'])==set([fixedsnps[site][gt[h].get(site,[])[i]] for i in range(2)]) for h in hybrids if gt[h].get(site,[])]):
+                f1het.append(site)
+        loci = f1het
+        print >> sys.stderr, '%s parent-fixed snps het in hybrids' % (len(f1het))        
+    else:
+        loci = fixedsnps.keys()
+
+
+    genotypes = {}
+    for indiv in gt.keys():
+        genotypes[indiv] = {}
+        for site in fixedsnps.keys():
+            g = ''.join(sorted([fixedsnps[site].get(allele,'') for allele in gt[indiv].get(site,[])]))
+            if len(g) == 2:
+                genotypes[indiv][site] = g
+    if remove_targets is not None:
+        for t in remove_targets:
+            del genotypes[t]
+
+    return loci, genotypes
+
+def output_cross_radtag_genotypes(loci,genotypes,filename,lg0='X'):
+    '''Given list loci and dictionary genotype per genotypes_by_parent, writes file <filename>
+    suitable for RQTL
+
+    overloads 20101202:
+    - if loci is a dict per maploci from load_cross_radtag_genotypes below, sort by map position in output
+    - if filename is not string, use as filehandle (permits passing sys.stdout, for instance)
+    '''
+
+    def sortkey(x):
+        if x == '':
+            return 0
+        else:
+            return x
+
+    if isinstance(loci,list):
+        locnames = loci
+        lgs = ['1' for i in range(len(loci))]
+        mps = [str(i+1) for i in range(len(loci))]
+    elif isinstance(loci,dict):
+        for k,v in loci.items():
+            if v[0] == 0:
+                loci[k] = (lg0,v[1])
+        locnames,lgs,mps = zip(*[(loc,str(lg),str(mp)) for loc,(lg,mp) in sorted(loci.items(),key=lambda x:[sortkey(v) for v in x[1]])])
+
+    mID_lookup = dict([(m,str(i)) for i,m in enumerate(sorted(genotypes.keys()))])
+
+    if isinstance(filename,str):
+        fh = open(filename ,'w')
+        #open(filename+'.mIDlookup','w').write('\n'.join(['%s\t%s' % (i,m) for m,i in sorted(mID_lookup.items())]))
+    else:
+        fh = filename
+        #open(filename.name+'.mIDlookup','w').write('\n'.join(['%s\t%s' % (i,m) for m,i in sorted(mID_lookup.items())]))
+        
+    fh.write('ID,')
+    fh.write(','.join(['%sr' % l for l in locnames]))
+    fh.write('\n')
+    fh.write(',')
+    fh.write(','.join(lgs))
+    fh.write('\n')
+    fh.write(',')
+    fh.write(','.join(mps))
+    fh.write('\n')
+
+    out_geno = {}
+    
+    for mID in genotypes.keys():
+        #fh.write(mID_lookup[mID]+',')
+        fh.write(mID+',')
+        out_geno[mID] = dict([(mkr,genotypes[mID][mkr]) for mkr in locnames if genotypes[mID].has_key(mkr)])
+        fh.write(','.join([genotypes[mID].get(mkr,'-') for mkr in locnames]))
+        fh.write('\n')
+
+    fh.close()
+
+    return out_geno,mID_lookup
+
+
 
 if __name__ == "__main__":
 
@@ -13,7 +312,8 @@ if __name__ == "__main__":
     site_before = 32 #polymorphism must occur before this base in a fragment
     #chi2crit = 30
     
-    vcfn,qd,gq,chi2crit = sys.argv[1:]
+    #vcfn,qd,gq,chi2crit = sys.argv[1:]
+    vcfn,qd,gq = sys.argv[1:]
     
     
     outbase = os.path.splitext(vcfn)[0]
@@ -22,16 +322,21 @@ if __name__ == "__main__":
 
 
     print >> sys.stderr, 'loading vcf',vcfn
-    vcf = variant_detection.load_vcf(vcfn,cutoff_fn=cut_fn,indiv_gt_phred_cut=float(gq))
+    vcf = load_vcf(vcfn,cutoff_fn=cut_fn,indiv_gt_phred_cut=float(gq))
 
     print >> sys.stderr, 'convert to pm/gt matrices'
-    pm,gt = extract_genotypes_from_mclgr.genotypes_from_vcf_obj(vcf)
+    pm,gt = genotypes_from_vcf_obj(vcf)
 
     parents_prefixes = dict(zip(['A', 'B'],parent_str.split(',')))
     parents = dict([(l,[k for k in gt.keys() if k.startswith(p)]) for l,p in parents_prefixes.items()])
 
-    polarized_loci,polarized_geno = extract_genotypes_from_mclgr.genotypes_by_parent(dict([(k,v) for k,v in pm.items() if int(k.split('.')[1]) < site_before]),gt,parents,remove_targets=reduce(lambda x,y: x+y,parents.values()))
+    polarized_loci,polarized_geno = genotypes_by_parent(dict([(k,v) for k,v in pm.items() if int(k.split('.')[1]) < site_before]),gt,parents,remove_targets=reduce(lambda x,y: x+y,parents.values()))
 
+    #chi2-free output:
+    ret = extract_genotypes_from_mclgr.output_cross_radtag_genotypes(polarized_loci,polarized_geno,'%s_QD%s-GQ%s_%sbp.csv' % (outbase,qd,gq,site_before))
+    
+
+    """ #ditch chi2
     print >> sys.stderr, 'filter X linked, chi2 critical %s' % chi2crit
     xsites,autsites = extract_genotypes_from_mclgr.filter_Xlinked_loci(polarized_loci, polarized_geno,float(chi2crit))
     print >> sys.stderr, '%s X linked, %s autosomal' % (len(xsites),len(autsites))
@@ -43,3 +348,4 @@ if __name__ == "__main__":
     print >> sys.stderr, '%s_QD%s-GQ%s_%sbp_Xchi%s.csv' % (outbase,qd,gq,site_before,chi2crit)
     print >> sys.stderr, '%s_QD%s-GQ%s_%sbp_autchi%s.csv' % (outbase,qd,gq,site_before,chi2crit)
     print >> sys.stderr, 'done'
+    """
