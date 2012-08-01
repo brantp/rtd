ced710b5c79341571edef2e941b9daf3c1bd737d
Brant Peterson <brantp@gmail.com>
2012-08-01 17:58:39 -0400

    bugfixes to vcf_to_rqtl.py

--- vcf_to_rqtl.py
+++ vcf_to_rqtl.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 '''invocation:
-vcf_to_rqtl.py path_to_vcf.vcf Q G
+vcf_to_rqtl.py path_to_vcf.vcf "P" Q G
 where:
+P = comma-separated pair of prefixes identifying cross founder species/strains
+    for instance, if founding parents of strain "BW" are BW01, BW02, BW03
+    and founding parents of strain "PO" are PO01,PO02,PO03,
+    this paramter would be "BW,PO"
 Q = GATK "QD" score threshold for inclusion
 G = individual genotype call quality threshold
 
@@ -9,7 +13,7 @@ G = individual genotype call quality threshold
 # functionality from short_read now copied here
 #from short_read_analysis import variant_detection,extract_genotypes_from_mclgr
 
-import os,sys
+import os,sys,re,numpy
 from collections import defaultdict
 
 def hwe(indiv_gt):
@@ -99,16 +103,11 @@ def load_vcf(vcf,cutoff_fn=None,ding_on=100000,store_only=None,indiv_gt_phred_cu
             continue
 
         if line.startswith('#CHROM'):
-            if write_thresholded_vcf is not None:
-                ofh.write(line)
+
             headers = line[1:].split()
             exp_elements = len(line.split())
             FORMAT = headers.index('FORMAT')
-            if write_fasta_base is not None: #DON'T INCLUDE DROP_INDIV
-                if drop_indiv is not None:
-                    indivs = list(set(headers[FORMAT+1:]) - set(drop_indiv))
-                else:
-                    indivs = headers[FORMAT+1:]
+
         elif line.startswith('#'):
             continue
         else:
@@ -171,8 +170,6 @@ def load_vcf(vcf,cutoff_fn=None,ding_on=100000,store_only=None,indiv_gt_phred_cu
                     sd = keep_sd
                 if len(sd) > 0:
                     vcf_data[key] = sd
-                if write_thresholded_vcf is not None:
-                    ofh.write('\t'.join(fields) + '\n')
 
     return vcf_data
 
@@ -304,16 +301,16 @@ def output_cross_radtag_genotypes(loci,genotypes,filename,lg0='X'):
 
 if __name__ == "__main__":
 
-    parent_str = 'BW,PO'
+    #parent_str = 'Ep,Ti'
     #qd = 6
     #gq = 20
     min_indiv = 50
     fh = 0.7
-    site_before = 32 #polymorphism must occur before this base in a fragment
+    site_before = 45 #polymorphism must occur before this base in a fragment
     #chi2crit = 30
     
     #vcfn,qd,gq,chi2crit = sys.argv[1:]
-    vcfn,qd,gq = sys.argv[1:]
+    vcfn,parent_str,qd,gq = sys.argv[1:]
     
     
     outbase = os.path.splitext(vcfn)[0]
@@ -333,7 +330,7 @@ if __name__ == "__main__":
     polarized_loci,polarized_geno = genotypes_by_parent(dict([(k,v) for k,v in pm.items() if int(k.split('.')[1]) < site_before]),gt,parents,remove_targets=reduce(lambda x,y: x+y,parents.values()))
 
     #chi2-free output:
-    ret = extract_genotypes_from_mclgr.output_cross_radtag_genotypes(polarized_loci,polarized_geno,'%s_QD%s-GQ%s_%sbp.csv' % (outbase,qd,gq,site_before))
+    ret = output_cross_radtag_genotypes(polarized_loci,polarized_geno,'%s_QD%s-GQ%s_%sbp.csv' % (outbase,qd,gq,site_before))
     
 
     """ #ditch chi2
